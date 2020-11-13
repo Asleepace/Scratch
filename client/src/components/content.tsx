d6c63fb729eabc3d5ccbb418167bc73c3b08b92e
Colin Teahan <colin_teahan@yahoo.com>
2020-11-13 01:49:40 -0800

    almost working

--- client/src/components/content.tsx
+++ client/src/components/content.tsx
@@ -8,18 +8,41 @@ import { markdown } from 'markdown'
 export type OnChangEvent = React.FormEvent<HTMLDivElement>
 
 export const Content: React.FC<{}> = () => {
-  const [data, setData] = useStickyState("", "data");
-  const onChange = (event: OnChangEvent) => {
-    setData(event.currentTarget.innerText)
+  const [text, setText] = useStickyState("", "text");
+  const [edit, setEdit] = React.useState(false)
+
+  const onChange = (e: OnChangEvent) => {
+    console.log('[content] saving text:', e)
+    const currentText = e.currentTarget.innerText
+    setText(currentText)
   }
-  return (
-    <div
-      dangerouslySetInnerHTML={{ __html: markdown.toHTML(data) }}
-      suppressContentEditableWarning={true}
-      onInput={onChange}
-      contentEditable={true}
-      id={'display-content'}
-      onBlur={onChange}
-    />
-  )
+  console.log('[Content] render:', { text, edit })
+  const props = { text, setEdit, onChange }
+  return edit ? <EditableContent {...props} /> : <DisplayedContent {...props} />
 }
+
+interface EditableProps {
+  onChange?: ((event: React.FormEvent<HTMLDivElement>) => void) | undefined,
+  setEdit: (edit: boolean) => void,
+  text: string
+}
+
+const EditableContent: React.FC<EditableProps> = ({ text, onChange, setEdit }) => (
+  <div
+    suppressContentEditableWarning={true}
+    onBlur={() => setEdit(false)}
+    onChange={onChange}
+    contentEditable={true}
+    defaultValue={text}
+    id={'display-content'}
+  />
+)
+
+const DisplayedContent: React.FC<EditableProps> = ({ text, setEdit }) => (
+  <div
+    dangerouslySetInnerHTML={{ __html: markdown.toHTML(text) }}
+    suppressContentEditableWarning={true}
+    onClick={() => setEdit(true)}
+    id={'display-content'}
+  />
+)
