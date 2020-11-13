86c96256256bbe099adf8923c101c1ee2711e8d7
Colin Teahan <colin_teahan@yahoo.com>
2020-11-13 02:13:52 -0800

    content editan;e class working

--- client/src/components/content.tsx
+++ client/src/components/content.tsx
@@ -4,19 +4,14 @@ import React from 'react'
 import { useStickyState } from '../api/storage'
 import { markdown } from 'markdown'
 
-
 export type OnChangEvent = React.FormEvent<HTMLDivElement>
 
 export const Content: React.FC<{}> = () => {
   const [text, setText] = useStickyState("", "text");
   const [edit, setEdit] = React.useState(false)
-
   const onChange = (e: OnChangEvent) => {
-    console.log('[content] saving text:', e)
-    const currentText = e.currentTarget.innerText
-    setText(currentText)
+    setText(e.currentTarget.innerText)
   }
-  console.log('[Content] render:', { text, edit })
   return (
     <div className={"content"}>
       <div
